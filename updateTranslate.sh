999c2f40fb270ae627e5b50b08ee5bd058aa1ffe
wusuopu <admin@longchangjin.cn>
2013-12-30 18:47:11 +0800

    update translate sh

--- updateTranslate.sh
+++ updateTranslate.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 # Update translation.
 
-MSGFMT=$(which msgfmt)
-if [ -z "$MSGFMT" ]; then
-    echo "You have 'gettext' installed to generate mo files."
-    exit 1
-fi
-
-for pofile in po/*.po; do
-    lang=$(basename $pofile .po)
-    dest="locale/$lang/LC_MESSAGES"
-    [[ -e "$dest" ]] || mkdir -p "$dest"
-    echo "$pofile --> $dest/deepin-screenshot.mo"
-    msgfmt $pofile -o "$dest/deepin-screenshot.mo"
-done
-
-## locale hack
-ln -sf en_US locale/default
+cd ./tools
+python generate_mo.py 
