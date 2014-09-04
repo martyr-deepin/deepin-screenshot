88e3b92f1f9ad55cee0ee83f0819a2d88b8382d1
snyh <snyh@snyh.org>
2014-09-04 16:48:32 +0800

    tidy mo files; fix po permission; update i18n tools
    Change-Id: Ib64f1cd56183de7f12c56acfbe47d98af9b00bf0

--- tools/generate_mo.py
+++ tools/generate_mo.py
@@ -3,20 +3,20 @@
 
 # Copyright (C) 2011 ~ 2012 Deepin, Inc.
 #               2011 ~ 2012 Wang Yong
-# 
+#
 # Author:     Wang Yong <lazycat.manatee@gmail.com>
 # Maintainer: Wang Yong <lazycat.manatee@gmail.com>
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -39,11 +39,19 @@ if __name__ == "__main__":
             "mkdir -p %s" % (os.path.join(locale_dir, "%s/LC_MESSAGES/" % (lang))),
             shell=True
             )
-        
+
         subprocess.call(
             "msgfmt -o %s %s" % (
                 os.path.join(locale_dir, "%s/LC_MESSAGES/%s.mo" % (lang, project_name)),
                 os.path.join(locale_dir, "%s.po" % (lang))),
             shell=True
             )
-    
+
+    for lang in langs:
+        subprocess.call(
+            "sudo cp -r %s %s" % (
+                os.path.join(locale_dir, "%s" % lang),
+                "/usr/share/locale/"),
+            shell=True
+            )
+
--- tools/update_pot.py
+++ tools/update_pot.py
@@ -3,20 +3,20 @@
 
 # Copyright (C) 2011 ~ 2012 Deepin, Inc.
 #               2011 ~ 2012 Wang Yong
-# 
+#
 # Author:     Wang Yong <lazycat.manatee@gmail.com>
 # Maintainer: Wang Yong <lazycat.manatee@gmail.com>
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -33,55 +33,69 @@ def remove_directory(path):
                 remove_directory(full_path)
             else:
                 os.remove(full_path)
-        os.rmdir(path)        
-        
+        os.rmdir(path)
+
 def create_directory(directory, remove_first=False):
     '''Create directory.'''
     if remove_first and os.path.exists(directory):
         remove_directory(directory)
-    
+
     if not os.path.exists(directory):
         os.makedirs(directory)
-        
-if __name__ == "__main__":
+
+def update_pot():
     # Read config options.
     config_parser = ConfigParser()
     config_parser.read("locale_config.ini")
     project_name = config_parser.get("locale", "project_name")
     source_dir = config_parser.get("locale", "source_dir")
     locale_dir = os.path.abspath(config_parser.get("locale", "locale_dir"))
-    langs = eval(config_parser.get("locale", "langs"))
     create_directory(locale_dir)
-    
+
     # Get input arguments.
-    source_files = []
+    include_qml = False
+    py_source_files = []
     for root, dirs, files in os.walk(source_dir):
         for each_file in files:
+            if each_file.endswith(".qml") and not include_qml:
+                include_qml = True
             if each_file.endswith(".py") and not each_file.startswith("."):
-                source_files.append(os.path.join(root, each_file))
-                
+                py_source_files.append(os.path.join(root, each_file))
+
     pot_filepath = os.path.join(locale_dir, project_name + ".pot")
-    
-    # Generate pot file.
-    subprocess.call(
-        "xgettext --from-code=UTF-8 -k_ -o %s %s" % (pot_filepath, ' '.join(source_files)),
-        shell=True)
-    
-    # Generate po files.
-    for lang in langs:
+    if os.path.exists(pot_filepath):
+        os.remove(pot_filepath)
+
+    if include_qml:
+        ts_filepath = os.path.join(locale_dir, project_name + ".ts")
+
+        # Generate ts file
         subprocess.call(
-            "msginit --no-translator -l %s.UTF-8 -i %s -o %s" % (lang, pot_filepath, os.path.join(locale_dir, "%s.po" % (lang))),
-            shell=True
-            )
-        
-    # Replace ASCII with UTF-8.
-    for lang in langs:
-        po_filepath = os.path.join(locale_dir, "%s.po" % (lang))
-        read_file = open(po_filepath, "r")
-        whole_thing = read_file.read()
-        read_file.close()
-        
-        write_file = open(po_filepath, "w")
-        whole_thing = whole_thing.replace("charset=ASCII", "charset=UTF-8");
-        write_file.write(whole_thing);
-        write_file.close()
+            "deepin-lupdate -recursive %s -ts %s" % (os.path.realpath(source_dir), ts_filepath),
+            shell=True)
+
+        # convert to pot file.
+        subprocess.call(
+            "lconvert -i %s -o %s" % (ts_filepath, pot_filepath),
+            shell=True)
+
+        # clean string
+        clean_str = ""
+        with open(pot_filepath) as fp:
+            for line in fp:
+                if not line.startswith("msgctxt"):
+                    clean_str += line
+
+        with open(pot_filepath, "wb") as fp:
+            fp.write(clean_str)
+
+    # Merge pot file.
+    if os.path.exists(pot_filepath):
+        command = "xgettext -j -k_ -o %s %s" % (pot_filepath, ' '.join(py_source_files))
+    else:
+        command = "xgettext -k_ -o %s %s" % (pot_filepath, ' '.join(py_source_files))
+    subprocess.call(command, shell=True)
+
+if __name__ == "__main__":
+    update_pot()
+
