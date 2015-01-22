18ea02824fb4aac1771632396acd9a74fee7511d
hualet <mr.asianwang@gmail.com>
2015-01-22 15:39:21 +0800

    add i18n.py

---
+++ src/i18n.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 ~ 2015 Deepin, Inc.
+#               2011 ~ 2015 Wang YaoHua
+#
+# Author:     Wang YaoHua <mr.asianwang@gmail.com>
+# Maintainer: Wang YaoHua <mr.asianwang@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gettext
+
+LOCALE_DIR="/usr/share/locale"
+
+_ = None
+try:
+    _ = gettext.translation("deepin-movie", LOCALE_DIR).gettext
+except Exception, e:
+    _ = lambda i : i
