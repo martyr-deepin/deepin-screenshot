34e575fecc0991d60fb4747372244d860e8f1cf3
penghui <penghui@linuxdeepin.com>
2015-03-02 16:41:55 +0800

    adjust ui details of editPoints' shadow effect
    Change-Id: Ica00f9cfb2239b64af9045b4e495133df8358644

--- src/drawing_utils.js
+++ src/drawing_utils.js
@@ -24,17 +24,20 @@ function isPointsSameY(points) {
     }
 }
 
-function draw_point(ctx, startX, startY, radius, offsetX, offsetY,shadowBlur, shadowColor) {
-    offsetX = typeof offsetX !== 'undefined' ? offsetX : 0
-    offsetY = typeof offsetY !== 'undefined' ? offsetY : 1
-    shadowBlur = typeof shadowBlur !== 'undefined' ? shadowBlur : 2
-    shadowColor = typeof shadowColor !== 'undefined' ? shadowColor : Qt.rgba(0, 0, 0, 0.3)
+function draw_point(ctx, startX, startY, radius) {
     ctx.beginPath()
     ctx.arc(startX, startY, radius, 0, Math.PI * 2, false)
-    ctx.shadowOffsetX = offsetX
-    ctx.shadowOffsetY = offsetY
-    ctx.shadowBlur = shadowBlur
-    ctx.shadowColor = shadowColor
+    ctx.shadowOffsetX = 0
+    ctx.shadowOffsetY = 1
+    ctx.shadowBlur = 3
+    ctx.shadowColor = Qt.rgba(0, 0, 0, 0.2)
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+    ctx.beginPath()
+    ctx.arc(startX, startY,radius - 1, 0, Math.PI * 2, false)
+    ctx.shadowBlur =0
+    ctx.shadowColor = "transparent"
     ctx.closePath()
     ctx.fill()
     ctx.stroke()
