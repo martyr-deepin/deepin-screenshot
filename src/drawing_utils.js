a9fb713358f318fde922f8ca653e9cf4a900fbe4
Hualet Wang <mr.asianwang@gmail.com>
2015-03-04 14:26:07 +0800

    adjust the blur effect of the canvas shapes
    Change-Id: I590702b702875cdb22ed78f575abbdac7e699d52

--- src/drawing_utils.js
+++ src/drawing_utils.js
@@ -27,14 +27,14 @@ function draw_point(ctx, startX, startY, radius) {
     ctx.beginPath()
     ctx.arc(startX, startY, radius, 0, Math.PI * 2, false)
     ctx.shadowOffsetX = 0
-    ctx.shadowOffsetY = 1
+    ctx.shadowOffsetY = 0
     ctx.shadowBlur = 2
-    ctx.shadowColor = Qt.rgba(0, 0, 0, 0.2)
+    ctx.shadowColor = "black"
     ctx.closePath()
     ctx.fill()
     ctx.stroke()
     ctx.beginPath()
-    ctx.arc(startX, startY,radius - 1, 0, Math.PI * 2, false)
+    ctx.arc(startX, startY, radius, 0, Math.PI * 2, false)
     ctx.shadowBlur =0
     ctx.shadowColor = "transparent"
     ctx.closePath()
@@ -44,9 +44,9 @@ function draw_point(ctx, startX, startY, radius) {
 
 function draw_line(isShadow, ctx, endX, endY, offsetX, offsetY,shadowBlur, shadowColor) {
     offsetX = typeof offsetX !== 'undefined' ? offsetX : 0
-    offsetY = typeof offsetY !== 'undefined' ? offsetY : 1
+    offsetY = typeof offsetY !== 'undefined' ? offsetY : 0
     shadowBlur = typeof shadowBlur !== 'undefined' ? shadowBlur : 2
-    shadowColor = typeof shadowColor !== 'undefined' ? shadowColor : Qt.rgba(0, 0, 0, 0.2)
+    shadowColor = typeof shadowColor !== 'undefined' ? shadowColor : "black"
     ctx.lineTo(endX, endY)
     ctx.shadowOffsetX = isShadow ? offsetX : 0
     ctx.shadowOffsetY = isShadow ? offsetY : 0
