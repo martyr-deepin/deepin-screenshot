da1ee50b3cf4ef4e7c35814bbd617029c7132019
pangpangpang3 <penghui@linuxdeepin.com>
2016-02-04 11:29:40 +0800

    improve copyright
    Change-Id: I10fd7b50b183d0f8364ce145266822c529fc3e6d

--- src/ShortcutsLabel.qml
+++ src/ShortcutsLabel.qml
@@ -1,3 +1,11 @@
+/**
+ * Copyright (C) 2015 Deepin Technology Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ **/
 import QtQuick 2.1
 
 Row {
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,24 +1,12 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Copyright (C) 2011 ~ 2015 Deepin, Inc.
-#               2011 ~ 2015 Wang YaoHua
 #
-# Author:     Wang YaoHua <mr.asianwang@gmail.com>
-# Maintainer: Wang YaoHua <mr.asianwang@gmail.com>
+# Copyright (C) 2015 Deepin Technology Co., Ltd.
 #
-# This program is free software: you can redistribute it and/or modify
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 
 import sys
 from cStringIO import StringIO
@@ -26,7 +14,7 @@ from contextlib import contextmanager
 
 @contextmanager
 def no_error_output():
-	_stderr = sys.stderr
-	sys.stderr = StringIO()
-	yield
-	sys.stderr = _stderr
+        _stderr = sys.stderr
+        sys.stderr = StringIO()
+        yield
+        sys.stderr = _stderr
