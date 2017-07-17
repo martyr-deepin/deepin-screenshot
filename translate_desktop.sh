8a5df5acd006376fc313b99111be245c65d5d85d
pangpangpang3 <penghui@linuxdeepin.com>
2017-07-17 09:07:21 +0800

    Modify: wrong name in translate_desktop file.
    Change-Id: I27108aa4506272e0e8e6a1990f0e6623a833006c

--- translate_desktop.sh
+++ translate_desktop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DESKTOP_SOURCE_FILE=deepin-image-viewer.desktop
+DESKTOP_SOURCE_FILE=deepin-screenshot.desktop
 DESKTOP_TS_DIR=translations/desktop/
 
 /usr/bin/deepin-desktop-ts-convert ts2desktop $DESKTOP_SOURCE_FILE $DESKTOP_TS_DIR $DESKTOP_SOURCE_FILE
