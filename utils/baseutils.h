73b5baf00471f92a927bd674bb6a810e4518f1ad
pangpangpang3 <penghui@linuxdeepin.com>
2017-09-18 14:57:00 +0800

    Update: license.
    Change-Id: I766caeeb9e0714b871ac1881b590a29ed118a896

--- utils/baseutils.h
+++ utils/baseutils.h
@@ -1,4 +1,23 @@
-﻿#ifndef BASEUTILS_H
+/*
+ * Copyright (C) 2017 ~ 2017 Deepin Technology Co., Ltd.
+ *
+ * Maintainer: Peng Hui<penghui@deepin.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BASEUTILS_H
 #define BASEUTILS_H
 
 #include <QCursor>
